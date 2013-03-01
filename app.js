3ec7d69be3fe66724584ab9bc4cab809d378ef2a
Koko Digital <info@kokodigital.co.uk>
2013-03-01 12:13:46 +0000

    fr

--- app.js
+++ app.js
@@ -7,21 +7,22 @@ Ext.Loader.setPath({
 
 Ext.application({
 	
-			 controllers: ["Main201"],
+			// controllers: ["Main201"],
 			// controllers: ["Main202"],
-			 //controllers: ["Main206"],
-			//controllers: ["Main209"],
+			 // controllers: ["Main206"],
+			// controllers: ["Main209"],
 			
 			name : 'MyApp',
 			requires : [ 'Ext.MessageBox' ],
 			
 			// views : [ 'Main2' ],
-			 views : [ 'Main201' ],
-			 //views : [ 'Main202' ],
-			 //views : [ 'Main203' ],
+			// views : [ 'Main201' ],
+			 // views : [ 'Main202' ],
+			 // views : [ 'Main203' ],
 			// views : [ 'Main204' ],
-			// views : [ 'Main205','Main205topdock','Main205bottomdock','Main205leftdock','Main205rightdock'],
-			 //views : [ 'Main206'],
+			// views : [
+			// 'Main205','Main205topdock','Main205bottomdock','Main205leftdock','Main205rightdock'],
+			 // views : [ 'Main206'],
 			// views : [ 'Main207','Main207toolbar'],
 			// views : [ 'Main208','Main208tbar','Main208bbar'],
 			  // views : [ 'Main209' ],
@@ -56,8 +57,74 @@ Ext.application({
 				// Ext.Viewport.add(Ext.create('MyApp.view.Main203'));
 				// Ext.Viewport.add(Ext.create('MyApp.view.Main204'));
 				// Ext.Viewport.add(Ext.create('MyApp.view.Main205'));
-				Ext.Viewport.add(Ext.create('MyApp.view.Main201'));
+				// Ext.Viewport.add(Ext.create('MyApp.view.Main201'));
 				
+var handleNavigation = function(btn) { 
+var currentContainer = myContainer.getActiveItem(), 
+innerItems = myContainer.getInnerItems(),
+totalItems = innerItems.length,
+currentIndex = innerItems.indexOf(currentContainer),
+direction,
+newIndex;
+if (btn.getText() == 'Back') { 
+direction = 'right';
+newIndex = currentIndex > 0
+? (currentIndex - 1) : (totalItems - 1);
+}
+else {
+direction = 'left';
+newIndex = currentIndex < (totalItems - 1)
+? (currentIndex + 1) : 0;
+}
+myContainer.animateActiveItem(newIndex, {
+type : 'slide',
+direction : direction
+});
+};
+var myContainer = Ext.create('Ext.Container',{
+fullscreen : true,
+activeItem : 1, 
+layout : {
+type : 'card', 
+animation : 'slide'
+},
+items : [
+{
+xtype : 'toolbar',
+docked : 'top',
+title : 'Card Layout',
+items : [
+{
+text : 'Back',
+ui : 'back',
+handler : handleNavigation 
+},
+{ xtype : 'spacer' },
+{
+text : 'Forward',
+ui : 'forward',
+handler : handleNavigation
+}
+]
+},
+{
+html : 'Card 1',
+style : 'background-color: #99F;'
+},
+{
+html : 'Card 2',
+style : 'background-color: #F99;'
+},
+{
+html : 'Card 3',
+style : 'background-color: #9F9;'
+}
+]
+});
+
+
+
+Ext.Viewport.add(myContainer)
 
 
 			},

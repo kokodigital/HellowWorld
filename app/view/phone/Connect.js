dd30107fc4a569027ec7d70862532548c4f87107
Koko Digital <info@kokodigital.co.uk>
2013-04-04 12:26:33 +0100

    dfg
    dfg

--- app/view/phone/Connect.js
+++ app/view/phone/Connect.js
@@ -1,101 +1,98 @@
-Ext
-		.define(
-				'QF.view.phone.Connect',
+Ext.define('QF.view.phone.Connect', {
+	extend : 'Ext.Panel',
+	xtype : 'connectview',
+	id : 'connectview',
+
+	config : {
+		flex : 1,
+		margin : '10px',
+		layout : {
+			type : 'vbox',
+			pack : 'center',
+			align : 'center'
+		},
+		items : [
 				{
-					extend : 'Ext.Panel',
-					xtype : 'connectview',
-					id : 'connectview',
-					initialize : function() {
-						this.callParent(arguments);
-						window.fbAsyncInit = Ext.bind(this.onFacebookInit, this);
+					xtype : 'button',
+					id : 'conNavloginFB',
+					text : 'Login with Facebook',
+					minWidth : '300px',
+					width : '100%',
+					margin : '5px',
+					listeners : {
+						tap : function(e) {
+							// this.fireEvent('loadViewEvent',
+							// this, 'Home', true, true, [
+							 //true, false ], [
+							// false, false ]);
+							try {
+								  FB.api('/platform', function(response) {
+									  console.log(response.company_overview);
+							
+							            Ext.Msg.alert('Success',  response.company_overview, Ext.emptyFn);
 
-					//	(function(d){
-						//	var js, id = 'facebook-jssdk'; if (d.getElementById(id)) {return;}
-						//	js = d.createElement('script'); js.id = id; js.async = true;
-						//	js.src = "//connect.facebook.net/en_US/all.js";
-						//	d.getElementsByTagName('head')[0].appendChild(js);
-						//}(document));
+
+									});
+
+							} catch (e) {
+								
 						
-						(function(d, s, id, debug){
-						 var js, fjs = d.getElementsByTagName(s)[0];
-						 if (d.getElementById(id)) {return;}
-						 js = d.createElement(s); js.id = id;
-						 js.src = "//connect.facebook.net/en_US/all" + (debug ? "/debug" : "") + ".js";
-						 fjs.parentNode.insertBefore(js, fjs);
-					   }(document, 'script', 'facebook-jssdk', /*debug*/ false));
-						
-						
-					},
+								 Ext.Msg.alert('Error',e, Ext.emptyFn);
+								 console.log(e);
+								 //console.log(window.PhoneGap.resources);
+							}
 
-					config : {
-						flex : 1,
-						margin : '10px',
-						layout : {
-							type : 'vbox',
-							pack : 'center',
-							align : 'center'
-						},
-						items : [
-								{
-									xtype : 'button',
-									id : 'conNavloginFB',
-									text : 'Login with Facebook',
-									minWidth : '300px',
-									width : '100%',
-									margin : '5px',
-									listeners : {
-										tap : function(e) {
-											// this.fireEvent('loadViewEvent',
-											// this, 'Home', true, true, [
-											// true, false ], [
-											// false, false ]);
-
-											FB.login(function(response) {
-												if (response.session) {
-													alert('logged in');
-												} else {
-													alert('not logged in');
-												}
-											}, {
-												scope : "email"
-											});
-
-										}
-									}
-								},
-								{
-									xtype : 'button',
-									id : 'conNavloginQF',
-									text : 'Login with Quiz Factor Account',
-									minWidth : '300px',
-									width : '100%',
-									margin : '5px',
-									listeners : {
-										tap : function(e) {
-											this.fireEvent('loadViewEvent',
-													this, 'Home', true, true, [
-															true, false ], [
-															false, false ]);
-										}
-									}
-								},
-								{
-									xtype : 'button',
-									id : 'conNavCreateAccount',
-									text : 'Create Account',
-									minWidth : '300px',
-									width : '100%',
-									margin : '5px',
-									listeners : {
-										tap : function(e) {
-											this.fireEvent('loadViewEvent',
-													this, 'Home', true, true, [
-															true, false ], [
-															false, false ]);
-										}
-									}
-								} ]
-
+						}
 					}
+				},
+				{
+					xtype : 'button',
+					id : 'conNavloginQF',
+					text : 'Login with Quiz Factor Account',
+					minWidth : '300px',
+					width : '100%',
+					margin : '5px',
+					listeners : {
+						tap : function(e) {
+							//this.fireEvent('loadViewEvent', this, 'Home', true,
+									//true, [ true, false ], [ false, false ]);
+							
+									
+						try {
+						    FB.init({
+					            appId: '628828893800003',
+					            nativeInterface: CDV.FB,
+					            useCachedDialogs: false
+					        });
 
-				});
+							} catch (e) {
+								
+						
+								 Ext.Msg.alert('Error',e, Ext.emptyFn);
+								 console.log(e);
+								 //console.log(window.PhoneGap.resources);
+							}
+
+
+
+						}
+					}
+				},
+				{
+					xtype : 'button',
+					id : 'conNavCreateAccount',
+					text : 'Create Account',
+					minWidth : '300px',
+					width : '100%',
+					margin : '5px',
+					listeners : {
+						tap : function(e) {
+							this.fireEvent('loadViewEvent', this, 'Home', true,
+									true, [ true, false ], [ false, false ]);
+						}
+					}
+				} ]
+
+	}
+
+});
